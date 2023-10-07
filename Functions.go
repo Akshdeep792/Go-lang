b6441bb14b375592d46947e07bbfc5786c3ef3f6
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2023-10-07 12:09:10 +0530

    Function Completed

--- Functions.go
+++ Functions.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //	func concat(s1 string, s2 string) string {
 //		return s1 + s2
 //	}
@@ -10,16 +8,43 @@ import "fmt"
 // 	return s1 + s2
 // }
 
-func getNames() (string, string) {
-	return "Akshdeep", "Singh"
-}
+// func getNames() (string, string) {
+// 	return "Akshdeep", "Singh"
+// }
+
+// ----------------->NAMED RETURN VALUES
+// func getDetails(age int) (tillAdult, tillDrink, tillRent int) {
+// 	tillAdult = 18 - age
+// 	if tillAdult < 0 {
+// 		tillAdult = 0
+// 	}
+
+// 	tillDrink = 21 - age
+// 	if tillDrink < 0 {
+// 		tillDrink = 0
+// 	}
+
+// 	tillRent = 25 - age
+// 	if tillRent < 0 {
+// 		tillRent = 0
+// 	}
+
+// 	return
+// }
 
 func main() {
 	// fmt.Println(concat("Hello", "World"))
 	// fmt.Println(concat2("Hello", "World", 21))
 
-	//ignoring last name
-	firstname, _ := getNames()
-	fmt.Println("Your Name", firstname)
+	//ignoring last name --->ignoring return value
+	// firstname, _ := getNames()
+	// fmt.Println("Your Name", firstname)
+
+	//-------------->Named return values
+	// tillAdult, tillDrink, tillRent := getDetails(13)
+	// fmt.Println("You are an adult in", tillAdult, "years")
+	// fmt.Println("You can drink in", tillDrink, "years")
+	// fmt.Println("You can rent a car in", tillRent, "years")
+	// fmt.Println("====================================")
 
 }
